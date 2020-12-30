b533c1737ed0312321720e3586aa203bbb2556e9
Guillem96 <guillem.orellana@gmail.com>
2020-12-30 17:11:17 +0100

    Lab 3 finished

--- docker_run.sh
+++ docker_run.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 DIR=$(pwd)
-if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-    VOLUME_DIR="$(wslpath -a -w "$DIR")"
-else
-    VOLUME_DIR=$DIR
-fi
+echo "Using volume dir: ${DIR}"
+
 viskit_port=$("$DIR/findport.py" 5000 1)
 
 xhost +local:root >/dev/null
 docker run --rm -p $viskit_port:$viskit_port -e VISKIT_PORT=$viskit_port \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v "$VOLUME_DIR":/root/code/bootcamp_pg \
+    -v ${DIR}:/root/code/bootcamp_pg \
     -ti dementrock/deeprlbootcamp \
         ${1-/bin/bash} "${@:2}"
 xhost -local:root >/dev/null
